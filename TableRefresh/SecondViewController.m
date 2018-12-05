2f001a27d6bb54a5fce8721bdf4e45d9163781f2
sunyong445 <87281923@qq.com>
2018-12-05 17:56:41 +0800

    Init
    Init

---
+++ TableRefresh/SecondViewController.m
@@ -0,0 +1,96 @@
+//
+//  SecondViewController.m
+//  TableRefresh
+//
+//  Created by Casey on 04/12/2018.
+//  Copyright © 2018 Casey. All rights reserved.
+//
+
+#import "SecondViewController.h"
+#import "CaseyRefresh.h"
+
+
+@interface SecondViewController ()<UITableViewDelegate, UITableViewDataSource>{
+    
+    IBOutlet UITableView *_tableview;
+    CyRefreshHeaderDefaultView *headview;
+    CyRefreshFooterDefaultView *footerview;
+}
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    _tableview.estimatedRowHeight = 0;
+    __weak typeof(self) weakSelf = self;
+    
+    headview = [CyRefreshHeaderDefaultView headerWithRefreshingBlock:^{
+        
+        NSLog(@"refresh ing");
+        [weakSelf performSelector:@selector(endFresh) withObject:nil afterDelay:5];
+        
+        
+    }];
+    _tableview.cy_header =headview;
+    _tableview.cy_header.backgroundColor = UIColor.redColor;
+   
+    
+    footerview= [CyRefreshFooterDefaultView footerWithRefreshingBlock:^{
+        
+        NSLog(@"footer refresh ing");
+        [weakSelf performSelector:@selector(endFresh) withObject:nil afterDelay:5];
+        
+    }];
+    _tableview.cy_footer = footerview;
+    
+    _tableview.cy_footer.backgroundColor = UIColor.yellowColor;
+    
+}
+
+- (void)endFresh{
+    
+        [_tableview.cy_header endRefreshing];
+        [_tableview.cy_footer endRefreshingNoMoreData];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    
+    return 5;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    
+    return 60;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    
+    UITableViewCell *cell = [[UITableViewCell alloc] init];
+    cell.textLabel.text = [@(indexPath.row) description];
+    return cell;
+    
+}
+
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    
+  
+    [self.navigationController pushViewController:[SecondViewController new] animated:YES];
+    
+}
+
+
+- (void)dealloc{
+    
+    NSLog(@"%s", __func__);
+}
+
+@end
